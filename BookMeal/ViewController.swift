cce0cdda3526ac138dcd25ad4af6e7c0f8babb29
Mac WuXinLing <218100345@qq.com>
2017-12-18 14:53:38 +0800

    首次提交

---
+++ BookMeal/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  BookMeal
+//
+//  Created by Mac WuXinLing on 2017/12/18.
+//  Copyright © 2017年 巫星林. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ Podfile
@@ -0,0 +1,9 @@
+platform :ios, '9.0'
+
+target 'BookMeal' do
+  # Comment the next line if you're not using Swift and don't want to use dynamic frameworks
+  use_frameworks!
+
+pod 'Alamofire', '~> 4.4.0'
+
+end
